b3efda710345a115067d9c9a6e74212435689b70
B8angho <bhjeon98@gmail.com>
2023-12-12 09:42:53 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import ToggleThemeButton from './component/ToggleThemeButton';
+import Hero from './component/Hero';
+import ResultContainer from './component/ResultContainer';
+import Footer from './component/Footer';
+import { useState, useEffect } from 'react';
+import './App.css';
+import getWallPapers from './api/getWallPapers';
+
+const Container = styled.div`
+    position: relative;
+    background-color: var(--primary);
+    min-height: 100vh;
+`;
+
+function App() {
+    const [data, setData] = useState({});
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        const fetch = async () => {
+            const data = await getWallPapers({
+                q: query,
+            });
+            setData(data);
+        };
+        fetch();
+    }, [query]);
+    return (
+        <>
+            <Container>
+                <Hero setQuery={setQuery} />
+                <ResultContainer data={data} />
+                <Footer />
+                <ToggleThemeButton />
+            </Container>
+        </>
+    );
+}
+
+export default App;
---
+++ src/component/ResultContainer.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import ImageCard from './ImageCard';
+import ImageModal from './ImageModal';
+import Pagination from './Pagination';
+import EmptyResult from './EmptyResult';
+import getWallPapers from '../api/getWallPapers';
+
+const Container = styled.div`
+    max-width: 1830px;
+    margin: 8px auto;
+    padding-right: 8px;
+`;
+
+const ResultsWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+`;
+
+const ResultContainer = ({ data }) => {
+    return (
+        <Container>
+            {/* ImgCard 클릭 시 해당 이미지의 정보로 ImageModal이 나타나야 합니다. */}
+            {/* <ImageModal /> */}
+            {data.hits?.length > 0 && <Pagination />}
+            <ResultsWrapper>
+                {data.hits?.length > 0 ? (
+                    data.hits?.map((imgData) => (
+                        <ImageCard key={imgData.id} imgData={imgData} />
+                    ))
+                ) : (
+                    <EmptyResult />
+                )}
+            </ResultsWrapper>
+        </Container>
+    );
+};
+
+export default ResultContainer;
